d528808d3ceee70dd95a73b664ba8c38a2419b69
cyklon73 <87936307+cyklon73@users.noreply.github.com>
2023-07-07 22:47:28 +0200

    Initial commit

---
+++ Serializer.java
@@ -0,0 +1,122 @@
+
+/**
+
+ The Serializer class provides methods to serialize and deserialize
+ various primitive data types in Java.
+ <p>This class allows for easy conversion of primitive data types
+ to byte arrays and vice versa, enabling efficient data storage,
+ transmission, and interoperability.</p>
+ @see <a href="https://github.com/cyklon73/Serializer">GitHub Repository</a>
+ @version 1.0.0
+ @since July 7, 2023
+ @author Cyklon73
+ */
+public class Serializer {
+
+    public static byte[] asArray(byte val) {
+        return new byte[] {val};
+    }
+
+    public static byte[] asArray(byte... val) {
+        return val;
+    }
+
+    public static byte[] serializeByte(byte val) {
+        return asArray(val);
+    }
+
+    public static byte deserializeByte(byte[] val) {
+        return val[0];
+    }
+
+    public static byte[] serializeShort(short val) {
+        return asArray(
+                (byte) (val >>> 8),
+                (byte) val);
+    }
+
+    public static short deserializeShort(byte[] val) {
+        return (short) ((0xff & val[0]) << 8 |
+                        (0xff & val[1]));
+    }
+
+    public static byte[] serializeInt(int val) {
+        return asArray(
+                (byte) (val >>> 24),
+                (byte) (val >>> 16),
+                (byte) (val >>> 8),
+                (byte) val);
+    }
+
+    public static int deserializeInt(byte[] val) {
+        return  (0xff & val[0]) << 24 |
+                (0xff & val[1]) << 16 |
+                (0xff & val[2]) << 8 |
+                (0xff & val[3]);
+    }
+
+    public static byte[] serializeLong(long val) {
+        return asArray(
+                (byte) (val >>> 56),
+                (byte) (val >>> 48),
+                (byte) (val >>> 40),
+                (byte) (val >>> 32),
+                (byte) (val >>> 24),
+                (byte) (val >>> 16),
+                (byte) (val >>> 8),
+                (byte) val);
+    }
+
+    public static long deserializeLong(byte[] val) {
+        return  (0xffL & val[0]) << 56 |
+                (0xffL & val[1]) << 48 |
+                (0xffL & val[2]) << 40 |
+                (0xffL & val[3]) << 32 |
+                (0xffL & val[4]) << 24 |
+                (0xffL & val[5]) << 16 |
+                (0xffL & val[6]) << 8 |
+                (0xffL & val[7]);
+    }
+
+
+    public static byte[] serializeFloat(float val) {
+        return serializeInt(Float.floatToIntBits(val));
+    }
+
+    public static float deserializeFloat(byte[] val) {
+        return Float.intBitsToFloat(deserializeInt(val));
+    }
+
+    public static byte[] serializeDouble(double val) {
+        return serializeLong(Double.doubleToLongBits(val));
+    }
+
+    public static double deserializeDouble(byte[] val) {
+        return Double.longBitsToDouble(deserializeLong(val));
+    }
+
+    public static byte[] serializeBool(boolean val) {
+        return asArray((byte) (val ? 1 : 0));
+    }
+
+    public static boolean deserializeBool(byte[] val) {
+        return val[0]==1;
+    }
+
+    public static byte[] serializeChar(char val) {
+        return serializeInt(val);
+    }
+
+    public static char deserializeChar(byte[] val) {
+        return (char) deserializeInt(val);
+    }
+
+    public static byte[] serializeString(String val) {
+        return val.getBytes();
+    }
+
+    public static String deserializeString(byte[] val) {
+        return new String(val);
+    }
+
+}
